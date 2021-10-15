cbc9d9e001fdd1e6cc64effbeb36a1eb5dfa9d11
RudolfFrederiksen <rudolf.frederiksen@gmail.com>
2021-10-15 16:26:09 +0200

    move mock data outside the app component

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,28 @@ import { IExpenseItem } from "./components/Expenses/ExpenseItem/ExpenseItem";
 import { ExpenseList } from "./components/Expenses/ExpenseList/ExpenseList";
 import { NewExpense } from "./components/Expenses/NewExpense/NewExpense";
 
+const defaultExpenses: Array<IExpenseItem> = [
+    {
+        id: "1",
+        date: new Date(2021, 9, 5),
+        title: "Theater",
+        amount: 30,
+    },
+    {
+        id: "2",
+        date: new Date(2020, 9, 11),
+        title: "Rain jacket",
+        amount: 75,
+    },
+    {
+        id: "3",
+        date: new Date(2019, 9, 10),
+        title: "Groceries",
+        amount: 50,
+    },
+];
+
 function App() {
-    const defaultExpenses: Array<IExpenseItem> = [
-        {
-            id: "1",
-            date: new Date(2021, 9, 5),
-            title: "Theater",
-            amount: 30,
-        },
-        {
-            id: "2",
-            date: new Date(2020, 9, 11),
-            title: "Rain jacket",
-            amount: 75,
-        },
-        {
-            id: "3",
-            date: new Date(2019, 9, 10),
-            title: "Groceries",
-            amount: 50,
-        },
-    ];
     const [expenses, setExpenses] = useState(defaultExpenses);
 
     const addExpense = (expenseItem: IExpenseItem) => {
