571220d9a7cdb2adf3ddeda34e6323e012fc210c
RudolfFrederiksen <rudolf.frederiksen@gmail.com>
2021-10-25 16:28:39 +0200

    better filter label

--- src/components/Expenses/ExpenseFilter/ExpenseFilter.tsx
+++ src/components/Expenses/ExpenseFilter/ExpenseFilter.tsx
@@ -15,7 +15,7 @@ const ExpenseFilter = (props: IExpenseFilterProps) => {
             <div className="control">
                 <label>Filter by year</label>
                 <select onChange={selectHandler} value={props.defaultYear}>
-                    <option value="">No filter</option>
+                    <option value="">All years</option>
                     <option value="2022">2022</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
