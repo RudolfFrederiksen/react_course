ffe458bef96fbcd8eccd43e973509dcbedb4c165
RudolfFrederiksen <rudolf.frederiksen@gmail.com>
2021-10-29 12:07:03 +0200

    fix app root element error fix unique list key prop format files

--- 02-debugging/src/App.js
+++ 02-debugging/src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     const addGoalHandler = (enteredText) => {
         setCourseGoals((prevGoals) => {
             const updatedGoals = [...prevGoals];
-            updatedGoals.unshift({ text: enteredText, id: "goal1" });
+            updatedGoals.unshift({ text: enteredText, id: new Date().getTime() });
             return updatedGoals;
         });
     };
@@ -32,10 +32,12 @@ const App = () => {
     }
 
     return (
-      <section id="goal-form">
-          <CourseInput onAddGoal={addGoalHandler} />
-      </section>
-      <section id="goals">{content}</section>
+        <div>
+            <section id="goal-form">
+                <CourseInput onAddGoal={addGoalHandler} />
+            </section>
+            <section id="goals">{content}</section>
+        </div>
     );
 };
 
